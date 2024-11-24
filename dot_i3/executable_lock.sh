051068290e686ec016c60efdb81fb134758705af
Mukesh Kumar <mkso13189@gmail.com>
2024-11-24 23:51:32 +0530

    Some minor updates

--- dot_i3/executable_lock.sh
+++ dot_i3/executable_lock.sh
@@ -1,13 +1,28 @@
 #!/bin/sh
-# revert() {
-#   # xset dpms 0 0 0
-#   xset -s off -dmps
-# }
-# trap revert HUP INT TERM
-# xset +dpms dpms 5 5 5
-# # i3lock -n -f -c 000000
-#
-# Using i3lock-color ----
-i3lock --nofork --ignore-empty-password --show-failed-attempts --clock --radius 135 --pass-media-key -n -i /home/mkso/Pictures/wallpapers/minimalistic/gruvbox_minimal_space.png -C
-# blurlock
-# revert
+
+# Lock screen script
+lock() {
+  i3lock --nofork \
+    --ignore-empty-password \
+    --show-failed-attempts \
+    --clock \
+    --radius 135 \
+    --pass-media-key \
+    -i /home/mkso/Pictures/wallpapers/minimalistic/gruvbox_minimal_space.png \
+    --centered
+}
+
+# Handle screen power management
+revert() {
+  xset dpms 0 0 0
+  xset -dpms
+  xset s off
+}
+
+# Set screen timeout (in seconds)
+xset +dpms dpms 300 300 300 # 5 minutes
+trap revert HUP INT TERM
+
+# Execute lock
+lock
+revert
--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -60,7 +60,7 @@ config.window_padding = {
 	left = 0,
 	right = 0,
 	top = 0,
-	bottom = 0,
+	bottom = 20,
 }
 
 config.text_background_opacity = 1
